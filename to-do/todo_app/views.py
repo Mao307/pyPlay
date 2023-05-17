7268ae7c55728ee80bc357002a540cb7e5e6390a
Mao307 <moawia.3ao@gmail.com>
2023-05-18 02:23:38 +0300

    created a todo list view that uses index.html tmeplate created a templates dir with tow tmeplates : base.html & index.html

--- to-do/todo_app/views.py
+++ to-do/todo_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import TodoList
 
-# Create your views here.
+
+class ListListView(ListView):
+    model = TodoList
+    template_name = "todo_app/index.html"
+    
