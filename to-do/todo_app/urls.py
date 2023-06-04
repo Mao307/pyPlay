2ed6f83de58d0bca7d561edc9052b9e706d5e800
Mao307 <moawia.3ao@gmail.com>
2023-06-04 22:56:49 +0300

    - error has been fixed. - using hyphen in routes is a must in Django when refrencing a url or defining a route. - now user can create , add , view mutiple list and thier itmes :)

--- to-do/todo_app/urls.py
+++ to-do/todo_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns= [
     path("", views.ListListView.as_view(), name ="index"),
     path("list/<int:list_id>/",views.ItemListView.as_view(), name ="list"),
     path("list/add", views.ListCreate.as_view(), name = "list-add"),
-    path("List/<int:List_id>/item/add/", views.ItemCreate.as_view(), name ="item-add"),
-    path("List/<int:list_id>/item/update", views.ItemUpdate.as_view(), name = "item_update"),
+    path("list/<int:list_id>/item/add/", views.ItemCreate.as_view(), name ="item-add"),
+    path("list/<int:list_id>/item/<int:pk>/", views.ItemUpdate.as_view(), name = "item-update"),
 
 ]
--- to-do/todo_app/views.py
+++ to-do/todo_app/views.py
@@ -1,63 +1,137 @@
-from django.views.generic import ListView,CreateView, UpdateView
-from .models import TodoList, TodoItem
+from django.urls import reverse
+
+
+from django.views.generic import (
+
+    ListView,
+
+    CreateView,
+
+    UpdateView,
+
+)
+
+from .models import TodoItem, TodoList
 
 
 class ListListView(ListView):
+
     model = TodoList
+
     template_name = "todo_app/index.html"
-    
+
+
 class ItemListView(ListView):
-    model = TodoList
+
+    model = TodoItem
+
     template_name = "todo_app/todo_list.html"
 
+
     def get_queryset(self):
+
         return TodoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
-    
-    def get_context_data(self):
-        context = super().get_context_data()
-        context["todo_list"] = TodoList.objects.get(id=self.kwargs["list_id"])
-        return context
-class ListCreate(CreateView):
-    model = TodoList
-    fields = ["title"]
+
 
     def get_context_data(self):
-        context =super(ListCreate,self).get_context_data()
-        context["title"] = "Add a new List"
+
+        context = super().get_context_data()
+
+        context["todo_list"] = TodoList.objects.get(id=self.kwargs["list_id"])
+
         return context
+
+
+class ListCreate(CreateView):
+
+    model = TodoList
+
+    fields = ["title"]
+
+
+    def get_context_data(self):
+
+        context = super(ListCreate, self).get_context_data()
+
+        context["title"] = "Add a new list"
+
+        return context
+
+
 class ItemCreate(CreateView):
-        model = TodoItem
-        fields = ["todo_list",
-                  "title",
-                  "description",
-                  "due_date",
-                  ]
-        def get_initial(self):
-            initial_data= super(ItemCreate, self).get_initial()
-            todo_list = TodoList.objects.get(id=self.kwargs["list_id"])
-            initial_data["todo_list"]= todo_list
-            return initial_data
-        def get_context_data(self):
-            context= super(ItemCreate, self).get_context_data()
-            todo_list = TodoList.objects.get(id = self.kwargs["List_id"])
-            context["todo_list"] = todo_list
-            context["title"] = "create a new item"
-            return context
-        def get_success_url(self):
-            return reverse("list", args=[self.object.todo_list_id])
-    
+
+    model = TodoItem
+
+    fields = [
+
+        "todo_list",
+
+        "title",
+
+        "description",
+
+        "due_date",
+
+    ]
+
+
+    def get_initial(self):
+
+        initial_data = super(ItemCreate, self).get_initial()
+
+        todo_list = TodoList.objects.get(id=self.kwargs["list_id"])
+
+        initial_data["todo_list"] = todo_list
+
+        return initial_data
+
+
+    def get_context_data(self):
+
+        context = super(ItemCreate, self).get_context_data()
+
+        todo_list = TodoList.objects.get(id=self.kwargs["list_id"])
+
+        context["todo_list"] = todo_list
+
+        context["title"] = "Create a new item"
+
+        return context
+
+
+    def get_success_url(self):
+
+        return reverse("list", args=[self.object.todo_list_id])
+
+
 class ItemUpdate(UpdateView):
-        model = TodoItem
-        fields = [
-            "todo_list",
-            "title",
-            "description",
-            "due_date"
-        ]
-        def get_context_data(self):
-            context = super(ItemUpdate, self).get_context_data()
-            context["todo_list"] = self.object.todo_list
-            context["title"] = "Edit item"
-            return context
-        def get_success_url(self):
-            return reverse("List", args=[self.object.todo_list_id])
+
+    model = TodoItem
+
+    fields = [
+
+        "todo_list",
+
+        "title",
+
+        "description",
+
+        "due_date",
+
+    ]
+
+
+    def get_context_data(self):
+
+        context = super(ItemUpdate, self).get_context_data()
+
+        context["todo_list"] = self.object.todo_list
+
+        context["title"] = "Edit item"
+
+        return context
+
+
+    def get_success_url(self):
+
+        return reverse("list", args=[self.object.todo_list_id])
